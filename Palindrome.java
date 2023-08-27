b536d9c587e96bd7a59ebfe0a72fdf8c548771c4
MaeYoiy <b6333034@gmail.com>
2023-08-27 21:26:19 +0700

    Solve Java Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,30 @@
 package LeetCode101;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Mink");
+        System.out.print("Enter your number: ");
+        int num = sc.nextInt();
+        System.out.print(isPalindrome(num));
+
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        // x not equal 0 or (first digit x not equal 0 and last digit not equal 0)
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int res = 0;
+        while (x > res) {
+            res = res * 10 + (x % 10);
+            x = x / 10;
+        }
+
+        return (x == res || x == res / 10);
     }
 }
